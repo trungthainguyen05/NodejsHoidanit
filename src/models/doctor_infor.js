d0d9d807e7cebe25273cb789a37a17218592f5eb
trungthainguyen05 <63950167+trungthainguyen05@users.noreply.github.com>
2022-11-18 15:50:25 +0700

    finish bai_78

---
+++ src/models/doctor_infor.js
@@ -0,0 +1,33 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Doctor_infor extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+
+        }
+    };
+    Doctor_infor.init({
+        // id: DataTypes.INTEGER,
+        doctorId: DataTypes.INTEGER,
+        priceId: DataTypes.STRING,
+        provinceId: DataTypes.STRING,
+        paymentId: DataTypes.STRING,
+        addressClinic: DataTypes.STRING,
+        nameClinic: DataTypes.STRING,
+        note: DataTypes.STRING,
+        count: DataTypes.INTEGER,
+
+
+
+    }, {
+        sequelize,
+        modelName: 'Doctor_infors',
+    });
+    return Doctor_infor;
+};
