50b28559993b5daadfb0318e82e871592fd53258
trungthainguyen05 <63950167+trungthainguyen05@users.noreply.github.com>
2022-11-21 15:48:36 +0700

    finish bai_84

--- src/migrations/migration-create-Booking.js
+++ src/migrations/migration-create-Booking.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
     up: async (queryInterface, Sequelize) => {
-        await queryInterface.createTable('Booking', {
+        await queryInterface.createTable('Bookings', {
             // statusId: DataTypes.STRING,
             // doctorId: DataTypes.STRING,
             // patientId: DataTypes.STRING,
@@ -25,7 +25,7 @@ module.exports = {
                 type: Sequelize.STRING
             },
             date: {
-                type: Sequelize.DATE
+                type: Sequelize.STRING
             },
             timeType: {
                 type: Sequelize.STRING
@@ -43,6 +43,6 @@ module.exports = {
         });
     },
     down: async (queryInterface, Sequelize) => {
-        await queryInterface.dropTable('Booking');
+        await queryInterface.dropTable('Bookings');
     }
 };
--- src/models/booking.js
+++ src/models/booking.js
@@ -18,12 +18,12 @@ module.exports = (sequelize, DataTypes) => {
         statusId: DataTypes.STRING,
         doctorId: DataTypes.STRING,
         patientId: DataTypes.STRING,
-        date: DataTypes.DATE,
+        date: DataTypes.STRING,
         timeType: DataTypes.STRING,
 
     }, {
         sequelize,
-        modelName: 'Booking',
+        modelName: 'Bookings',
     });
     return Booking;
 };
