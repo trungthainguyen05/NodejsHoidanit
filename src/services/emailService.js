f09aa3d0504a566c9dfcb6dbb78f95ca7fca7b00
trungthainguyen05 <63950167+trungthainguyen05@users.noreply.github.com>
2022-11-22 10:19:05 +0700

    init bai_87

---
+++ src/services/emailService.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+import nodemailer from 'nodemailer';
+// const nodemailer = require("nodemailer");
+
+let sendSimpleEmailService = async (dataSend) => {
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.EMAIL_APP, // generated ethereal user
+            pass: process.env.EMAIL_APP_PASSWORD, // generated ethereal password
+        },
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Trung 👻" <fakeEmail@gmail.com>', // sender address
+        to: dataSend.receiverEmail, // list of receivers
+        subject: "Thông tin đặt lịch khám bệnh", // Subject line
+        html: `
+        <h3>Xin chào ${dataSend.patientName}!</h3>
+        <p>Bạn nhận được email này vì đã đặt lịch khám bệnh online trên website designed by Trung</p>
+        <p>Thông tin đặt lịch khám bệnh: </p>
+        <div><b>Thời gian: ${dataSend.time}</b></div>
+        <div><b>Bác sĩ: ${dataSend.doctorName}</b></div>
+
+        <p>Nếu các thông tin trên là đúng sự thật, vui lòng click vào đường link bên dưới
+        để xác nhận và hoàn tất thủ tục đặt lịch khám bệnh.</p>
+        <div>
+        <a href="${dataSend.redirectLink} target = "_blank" >Click here</a>
+        </div>
+
+        <div>Xin chân thành cảm ơn</div>
+        `, // html body
+    });
+}
+
+
+module.exports = {
+    sendSimpleEmailService: sendSimpleEmailService,
+
+}
